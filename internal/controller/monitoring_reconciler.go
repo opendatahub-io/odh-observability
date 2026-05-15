6d1cf8cb55aa47a5e2f5167bb369df4a5019bfa9
Corey Goodfred <cgoodfre@redhat.com>
2026-05-14 22:09:39 -0400

    fix(gc): pass namespace to GC collector for cluster-scoped Monitoring CR

--- internal/controller/monitoring_reconciler.go
+++ internal/controller/monitoring_reconciler.go
@@ -244,6 +244,7 @@ func (r *MonitoringReconciler) collectGarbage(ctx context.Context, monitoring *v
 	collector := gc.New(
 		gc.WithOnlyCollectOwned(false),
 		gc.WithLabel(odhLabels.PlatformPartOf, "monitoring"),
+		gc.InNamespace(monitoring.Spec.Namespace),
 		gc.WithObjectPredicate(func(_ gc.RunParams, obj unstructured.Unstructured) (bool, error) {
 			k := resourceKey{
 				gvk:       obj.GroupVersionKind(),
@@ -270,6 +271,7 @@ func (r *MonitoringReconciler) deleteAllOwned(ctx context.Context, monitoring *v
 	collector := gc.New(
 		gc.WithOnlyCollectOwned(false),
 		gc.WithLabel(odhLabels.PlatformPartOf, "monitoring"),
+		gc.InNamespace(monitoring.Spec.Namespace),
 		gc.WithObjectPredicate(func(_ gc.RunParams, _ unstructured.Unstructured) (bool, error) {
 			return true, nil
 		}),
